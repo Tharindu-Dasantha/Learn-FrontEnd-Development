ee3960fed4c1c10e4211479ea80f993c11761b7b
Dasantha Edirisinghe <t.dasanthaedirisinghe@gmail.com>
2024-01-23 10:01:41 +0530

    Created

--- React/Simple-portfolio/src/Sections/Blog/Blog.jsx
+++ React/Simple-portfolio/src/Sections/Blog/Blog.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from "react";
 
 function Blog() {
   return (
-    <div>Blog</div>
-  )
+    <div className="flex flex-col justify-center items-center py-20">
+      <h2 className="font-extrabold text-6xl mb-10">Blog</h2>
+      <div className="flex flex-wrap justify-center items-center w-[60%] gap-5">
+        <div className="flex w-[500px] bg-slate-300 rounded-md">
+          <div className="w-[50%]">
+            {/* <img src="" alt="" /> */}
+          </div>
+          <div className="w-[50%] py-5">
+            <h3 className="font-bold">Learn HTML</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius eum
+              ipsum error voluptas nisi provident, harum architecto
+              reprehenderit necessitatibus voluptatum!
+            </p>
+          </div>
+        </div>
+        <div className="flex w-[500px] bg-slate-300 rounded-md">
+          <div className="w-[50%] h-2">
+            {/* <img src="" alt="" /> */}
+          </div>
+          <div className="w-[50%] py-5">
+            <h3 className="font-bold">Learn HTML</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius eum
+              ipsum error voluptas nisi provident, harum architecto
+              reprehenderit necessitatibus voluptatum!
+            </p>
+          </div>
+        </div>
+        <div className="flex w-[500px] bg-slate-300 rounded-md">
+          <div className="w-[50%] h-2 ">
+            {/* <img src="" alt="" /> */}
+          </div>
+          <div className="w-[50%] py-5">
+            <h3 className="font-bold">Learn HTML</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius eum
+              ipsum error voluptas nisi provident, harum architecto
+              reprehenderit necessitatibus voluptatum!
+            </p>
+          </div>
+        </div>
+        <div className="flex w-[500px] bg-slate-300 rounded-md">
+          <div className="w-[50%] h-2">
+            {/* <img src="" alt="" /> */}
+          </div>
+          <div className="w-[50%] py-5">
+            <h3 className="font-bold">Learn HTML</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius eum
+              ipsum error voluptas nisi provident, harum architecto
+              reprehenderit necessitatibus voluptatum!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Blog
+export default Blog;
--- React/Simple-portfolio/src/Sections/Services/Services.jsx
+++ React/Simple-portfolio/src/Sections/Services/Services.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+import BrushIcon from "@mui/icons-material/Brush";
+import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
 
 function Services() {
   return (
-    <div className='w-screen h-screen bg-[#DEE2E6]'>Services</div>
-  )
+    <div className="w-screen py-20 bg-[#DEE2E6]">
+      <div className="h-full flex flex-col items-center justify-center">
+        <h2 className="font-extrabold text-6xl mb-4">Services</h2>
+        <p className="w-[60vw] text-lg text-center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
+          explicabo voluptate asperiores ut dolorem nulla laborum, quia dolor
+          corrupti quidem modi pariatur quo nam natus officia laudantium,
+          blanditiis tempora commodi vel quos! Aut ullam ratione natus deserunt
+          ex, iste nemo.
+        </p>
+        <div className="flex gap-[50px] mt-6">
+          <div className="bg-black text-white py-4 px-2 text-center w-[250px] rounded-lg">
+            <DeveloperModeIcon className="mb-4 mt-3" />
+            <h3 className="text-xl font-bold">Web Development</h3>
+            <p className="text-sm mt-3 mb-4 w-[70%] m-auto">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe
+              dolorum, commodi nulla perspiciatis odio totam.
+            </p>
+          </div>
+          <div className="bg-black text-white py-4 px-2 text-center w-[250px] rounded-lg">
+            <BrushIcon className="mb-4 mt-3" />
+            <h3 className="text-xl font-bold">Web Design</h3>
+            <p className="text-sm mt-3 mb-4 w-[70%] m-auto">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem,
+              quibusdam neque dolore placeat velit non?
+            </p>
+          </div>
+          <div className="bg-black text-white py-4 px-2 text-center w-[250px] rounded-lg">
+            <PhoneIphoneIcon className="mb-4 mt-3" />
+            <h3 className="text-xl font-bold">App Design</h3>
+            <p className="text-sm mt-3 mb-4 w-[70%] m-auto">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo
+              optio quibusdam voluptatibus dicta ut ducimus.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Services
+export default Services;
